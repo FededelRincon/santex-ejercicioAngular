4759c167a7b8aebf14f50bd694a1303ba0a89877
FededelRincon <delrincon@gmail.com>
2023-07-12 22:02:07 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+
+interface Item {
+  position: number;
+  name: string;
+  cost: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practica-angular';
+
+  showList:boolean = true;
+
+  displayedColumns: string[] = ['position', 'name', 'cost'];
+
+  items:Item[] = [
+    { position: 1, name: 'Producto 1', cost: '$10' },
+    { position: 2, name: 'Producto 2', cost: '$20' },
+    { position: 3, name: 'Producto 3', cost: '$30' },
+    { position: 4, name: 'Producto 4', cost: '$40' },
+    { position: 5, name: 'Producto 5', cost: '$50' },
+  ];
+
+  // funcion mostrar/ocultar lista
+  toggleList() {
+    this.showList = !this.showList;
+  };
+
+
 }
